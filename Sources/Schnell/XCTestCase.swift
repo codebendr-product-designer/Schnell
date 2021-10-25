286736e5b67908be6b303daaaf0be318e3ba6736
codebendr-product-designer <codebendr@gmail.com>
2021-10-25 17:51:07 +0000

    adding XCTestCase print function

---
+++ Sources/Schnell/XCTestCase.swift
@@ -0,0 +1,11 @@
+import Foundation
+import XCTest
+
+public extension XCTestCase {
+    func example(_ title: String,
+                        code: () throws -> Void) rethrows {
+        print("## \(title)")
+      try code()
+        print("---")
+    }
+}
