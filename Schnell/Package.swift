a65d2b1997e53459c21c7b9564b9d7d80d3a1e1d
codebendr-product-designer <codebendr@gmail.com>
2021-12-28 22:41:13 +0000

    added reactive and functional swift modules

--- Schnell/Package.swift
+++ Schnell/Package.swift
@@ -6,13 +6,17 @@ let package = Package(
     name: "Schnell",
     products: [
         .library(name: "Schnell", targets: ["Schnell"]),
-        .library(name: "Koch", targets: ["Koch"])
+        .library(name: "Koch", targets: ["Koch"]),
+        .library(name: "ReactiveSwift", targets: ["ReactiveSwift"]),
+        .library(name: "FunctionalSwift", targets: ["FunctionalSwift"])
     ],
     dependencies: [
         .package(name: "HTML", url: "https://github.com/robb/Swim.git", .branch("main")),
     ],
     targets: [
         .target( name: "Schnell"),
+        .target( name: "FunctionalSwift"),
+        .target( name: "ReactiveSwift"),
         .target(name: "Koch", dependencies: [
             .product(name: "Swim", package: "HTML"),
             .product(name: "HTML", package: "HTML"),
