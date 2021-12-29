5030f67332842eafbaf3e8b494ff9850445cf4e8
codebendr-product-designer <codebendr@gmail.com>
2021-12-29 07:03:13 +0000

    moved subscription logic into  function for readability

--- Schnell/Sources/ReactiveSwift/plaground.playground/Contents.swift
+++ Schnell/Sources/ReactiveSwift/plaground.playground/Contents.swift
@@ -4,26 +4,22 @@ import UIKit
 import Combine
 import ReactiveSwift
 
-var seconds = 10
-
 
 var model = Model()
 var cancellables: Set<AnyCancellable> = []
-Timer.every(1.seconds) {
-    seconds -= 1
-    model.value += 2
-    if seconds == 0 {
-        $0.invalidate()
-    }
-}
+model.next()
 
+func subscription() -> AnyCancellable {
 model.$value
     .dropFirst()
     .map(\.description)
     .sink {
     print("number "+$0)
+   }
 }
-.store(in: &cancellables)
+   
+subscription()
+    .store(in: &cancellables)
 
 
 
