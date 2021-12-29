40e74595d74a0a98235d54499051e29438bf7146
codebendr-product-designer <codebendr@gmail.com>
2021-12-29 17:50:39 +0000

    download an image and applied some filters using functional programming concepts

--- Schnell/Sources/FunctionalSwift/Filter.playground/Contents.swift
+++ Schnell/Sources/FunctionalSwift/Filter.playground/Contents.swift
@@ -1,20 +1,31 @@
 import CoreImage
 import PlaygroundSupport
 import UIKit
-
-
+import FunctionalSwift
 
 
     class ViewController: UIViewController {
         override func loadView() {
+            
+            let url = URL(string: "https://picsum.photos/500/500")!
+            let ciimage = CIImage(contentsOf: url)!
+            
+            let radius = 5.0
+            let color = UIColor.red.withAlphaComponent(0.2)
+            let blurredImage = ImageFilter.blur(radius: radius)(ciimage)
+           let overlaidImage = ImageFilter.overlay(color: color)(blurredImage)
+            
             let view = UIView(frame: .init(x: 0, y: 0, width: 400, height: 400))
             view.backgroundColor = .white
 
             let image = UIImageView(frame: .init(x: 0, y: 0, width: 200, height: 200))
             image.contentMode = .scaleToFill
-            image.image = .init(named: "tomato")
+            image.image = .init(ciImage: overlaidImage)
 
             view.addSubview(image)
+            
+            
+            
             self.view = view
         }
     }
--- Schnell/Sources/FunctionalSwift/ImageFilter.swift
+++ Schnell/Sources/FunctionalSwift/ImageFilter.swift
@@ -2,10 +2,9 @@ import CoreImage
 import Foundation
 import UIKit
 
+public typealias Filter = (CIImage) -> CIImage
 public struct ImageFilter {
-    typealias Filter = (CIImage) -> CIImage
-
-    static func blur(radius: Double) -> Filter {
+    public static func blur(radius: Double) -> Filter {
         { image in
             let parameters: [String: Any] = [
                 kCIInputRadiusKey: radius,
@@ -24,7 +23,7 @@ public struct ImageFilter {
         }
     }
 
-    static func generate(color: UIColor) -> Filter {
+    public static func generate(color: UIColor) -> Filter {
         { _ in
             let parameters = [kCIInputColorKey: CIColor(cgColor: color.cgColor)]
             guard let filter = CIFilter(name: "CIConstantColorGenerator",
@@ -36,14 +35,14 @@ public struct ImageFilter {
         }
     }
 
-    static func compositeSourceOver(overlay: CIImage) -> Filter {
+    public static func compositeSourceOver(overlay: CIImage) -> Filter {
         return { image in
             let parameters = [
                 kCIInputBackgroundImageKey: image,
                 kCIInputImageKey: overlay
             ]
-            guard let filter = CIFilter(name: "CISourceOverCompositing",
-                                        parameters: parameters)
+            guard let filter = CIFilter(name: "CISourceOverCompositing", parameters: parameters)
+
             else { fatalError() }
             guard let outputImage = filter.outputImage
             else { fatalError() }
@@ -51,10 +50,16 @@ public struct ImageFilter {
         }
     }
 
-    func overlay(color: UIColor) -> Filter {
+    public static func overlay(color: UIColor) -> Filter {
         return { image in
-            let overlay = generate(color: color)(image).cropped(to: image.extent)
-            return compositeSourceOver(overlay: overlay)(image)
+            let overlay = ImageFilter.generate(color: color)(image).cropped(to: image.extent)
+            return ImageFilter.compositeSourceOver(overlay: overlay)(image)
         }
+    }
+
+    public static func compose(filter filter1: @escaping Filter,
+                 with filter2: @escaping Filter) -> Filter
+    {
+        return { image in filter2(filter1(image)) }
     }
 }
