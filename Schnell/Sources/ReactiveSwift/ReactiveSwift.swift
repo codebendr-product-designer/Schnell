9e261b7e333e03bb4fd18c2d810990b947072658
codebendr-product-designer <codebendr@gmail.com>
2021-12-29 07:02:46 +0000

    updated model with public access modifiers and extensions for code readability and access

--- Schnell/Sources/ReactiveSwift/ReactiveSwift.swift
+++ Schnell/Sources/ReactiveSwift/ReactiveSwift.swift
@@ -2,11 +2,24 @@ import Foundation
 import Combine
 
 public class Model {
-    @Published private(set) var value = 0
+    @Published public private(set) var value = 0
+    private var seconds = 10
     
     public init(){}
     
     public init(value: Int) {
         self.value = value
+    }
+}
+
+extension Model {
+    public func next() {
+        Timer.every(1.seconds) { [unowned self] in
+            self.seconds -= 1
+            value += 2
+            if seconds == 0 {
+                $0.invalidate()
+            }
+        }
     }
 }
