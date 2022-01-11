9268a22ff87c5b496a25672ceba0777a7d8ea5b8
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-01-11 23:10:31 +0000

    applied map transformation to int sequence publisher

--- Schnell/Sources/ReactiveSwift/Combine.playground/Pages/Publishers.xcplaygroundpage/Contents.swift
+++ Schnell/Sources/ReactiveSwift/Combine.playground/Pages/Publishers.xcplaygroundpage/Contents.swift
@@ -2,7 +2,19 @@ import Combine
 
 var cancellables: Set<AnyCancellable> = []
 [1, 2, 3, 4].publisher
-    .sink {
+    .map { "number \($0)" }
+    .sink(receiveCompletion: {
         print($0)
-    }
+    }, receiveValue: {
+        print($0)
+    })
     .store(in: &cancellables)
+
+class User {
+    var email = "unassigned"
+}
+
+
+var user = User()
+["test@email.com"].publisher.assign(to: \.email, on: user)
+user.email
