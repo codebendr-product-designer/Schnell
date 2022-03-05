1e7a68878738e3e8494959e246283c4f50780a79
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-03-05 17:19:22 +0000

    updated SPM package and some auto IDE updates as a result

--- Package.swift
+++ Package.swift
@@ -14,19 +14,25 @@ let package = Package(
         .library(name: "ReactiveSwift", targets: ["ReactiveSwift"]),
         .library(name: "FunctionalSwift", targets: ["FunctionalSwift"]),
         .library(name: "Kabel", targets: ["Kabel"]),
+        
         //Dependency Design
         .library(name: "WeatherClient", targets: ["WeatherClient"]),
         .library(name: "WeatherClientLive", targets: ["WeatherClientLive"]),
         .library(name: "WeatherFeature", targets: ["WeatherFeature"]),
+        .library(name: "PathMonitor", targets: ["PathMonitor"]),
+        .library(name: "PathMonitorLive", targets: ["PathMonitorLive"]),
         .library(name: "DesignPatterns", targets: ["DesignPatterns"]),
+        
     ],
     dependencies: [
         .package(name: "HTML", url: "https://github.com/robb/Swim.git", .branch("main")),
     ],
     targets: [
+        .target(name: "PathMonitor"),
+        .target(name: "PathMonitorLive", dependencies: ["PathMonitor"]),
         .target(name: "WeatherClient"),
         .target(name: "WeatherClientLive", dependencies: ["WeatherClient"]),
-        .target(name: "WeatherFeature", dependencies: ["WeatherClientLive"]),
+        .target(name: "WeatherFeature", dependencies: ["WeatherClientLive", "PathMonitorLive"]),
         .target(name: "DesignPatterns"),
         .target(name: "Kabel"),
         .target(name: "Schnell"),
