a1789e486ea8e8f59834ec1a34fdf0069c7f31ab
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-03-07 20:18:15 +0000

    added CoreLocation delegate methods

--- Sources/WeatherFeature/AppViewModel.swift
+++ Sources/WeatherFeature/AppViewModel.swift
@@ -4,12 +4,14 @@ import WeatherClient
 import PathMonitor
 import CoreLocation
 
-public class AppViewModel: ObservableObject {
+public class AppViewModel: NSObject, ObservableObject {
     @Published var isConnected = false
     @Published var weatherResults = [WeatherResponse.ConsolidatedWeather]()
     private var cancellables: Set<AnyCancellable> = []
     private let weatherClient: WeatherClient
     private let pathMonitorClient: PathMonitor
+    private var currentLocation: Location?
+    let locationManager = CLLocationManager()
     
     let dayOfWeekFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -23,6 +25,8 @@ public class AppViewModel: ObservableObject {
     ){
         self.weatherClient = weatherClient
         self.pathMonitorClient = pathMonitorClient
+        super.init()
+        
         pathMonitorClient.publisher
             .map { $0.status == .satisfied }
             .removeDuplicates()
@@ -36,11 +40,17 @@ public class AppViewModel: ObservableObject {
                 }
             }
             .store(in: &cancellables)
+        
+        locationManager.delegate = self
     }
     
     func refresh() {
+        guard let currentLocation = currentLocation else {
+            return
+        }
+
         self.weatherResults = []
-        self.weatherClient.weather(2459115)
+        self.weatherClient.weather(currentLocation.woeid)
             .sink(
                 receiveCompletion: { _ in },
                 receiveValue: { response in
@@ -53,7 +63,7 @@ public class AppViewModel: ObservableObject {
     }
     
     func locationButtonTapped() {
-        let locationManager = CLLocationManager()
+
         switch locationManager.authorizationStatus {
         case .notDetermined:
             locationManager.requestWhenInUseAuthorization()
@@ -69,4 +79,45 @@ public class AppViewModel: ObservableObject {
             break
         }
     }
+    
+   
+}
+
+extension AppViewModel: CLLocationManagerDelegate {
+    
+    public func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            break
+        case .restricted:
+            //TODO show an alert
+            break
+        case .denied:
+            //TODO show an alert
+            break
+        case .authorizedAlways, .authorizedWhenInUse, .authorized:
+            locationManager.requestLocation()
+        @unknown default:
+            break
+        }
+    }
+    
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else { return }
+        weatherClient.searchLocations(location.coordinate)
+            .receive(on: DispatchQueue.main)
+            .sink(
+                receiveCompletion: { _ in },
+                receiveValue: { [weak self] location in
+                    self?.currentLocation  = location.first
+                    self?.refresh()
+                }
+            )
+            .store(in: &cancellables)
+    }
+    
+    public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
+    }
+    
 }
